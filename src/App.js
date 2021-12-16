81c9e349805b5e43178c935434513e969bc8a1d4
AayushMohan <aayushmohan1702@gmail.com>
2021-12-16 20:51:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import ExpenseItem from "./Components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>This is also visible!</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
-
 export default App;
