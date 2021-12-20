5fb67bd67247c43aa8e169852d6445dcd38d325c
AayushMohan <aayushmohan1702@gmail.com>
2021-12-21 02:30:03 +0530

    commit

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -2,9 +2,17 @@ import React from "react";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const month = props.date.toLocaleString("en-us", { month: "long" }); // "en-us" is the locale for US
+  const day = props.date.getDate(); // getDate() returns the day of the month
+  const year = props.date.getFullYear(); // getFullYear() returns the year
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
 
       <div className="expense-item__description">
         <h2>{props.title}</h2>
