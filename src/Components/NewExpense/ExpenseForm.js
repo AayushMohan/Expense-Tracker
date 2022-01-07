a0998bc0a0fa2b16c0d907e2c12f09c7ead2fc2f
AayushMohan <aayushmohan1702@gmail.com>
2022-01-07 18:58:59 +0530

    commit

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -95,6 +95,7 @@ const ExpenseForm = (props) => {
         </div>
         <div className="new-expense__actions">
           <button type="submit">Add Expense</button>
+          <button type="cancel">Cancel</button>
         </div>
       </form>
     </div>
