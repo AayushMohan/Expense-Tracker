64e969c4c8e8bb36894a153b21fa618ee90fdf2d
AayushMohan <aayushmohan1702@gmail.com>
2022-01-08 18:22:24 +0530

    commit

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -3,7 +3,13 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
-  return <div className="chart"></div>;
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar />
+      ))}
+    </div>
+  );
 };
 
 export default Chart;
