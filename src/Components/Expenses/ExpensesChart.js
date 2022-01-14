bf7b743666a40ac2be975b116f3f47f9875715d7
AayushMohan <aayushmohan1702@gmail.com>
2022-01-14 17:28:33 +0530

    commit

--- src/Components/Expenses/ExpensesChart.js
+++ src/Components/Expenses/ExpensesChart.js
@@ -22,7 +22,7 @@ const ExpensesChart = (props) => {
     chartDataPoints[expenseMonth].value += expense.amount;
   }
 
-  return <Chart dataPoints />;
+  return <Chart dataPoints={chartDataPoints} />;
 };
 
 export default ExpensesChart;
